dfcc7239a61ea196144d745ff397d8c93b6b019f
Jesse Mihalik-Howard <jessehoward@Jesses-MBP-M1.hsd1.co.comcast.net>
2022-06-03 11:00:16 -0600

    initial setup and directories

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Container, Row, Col} from "reactstrap";
+import './App.css';
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello World...</h1>
+      <Row>
+        <Col>Col 1</Col>
+        <Col>Col 1</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
